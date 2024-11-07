84666f33026bd3e8cad5861dd33381d688f33ede
20551a0231-Sruthi <20551a0231.sruthi@gmail.com>
2024-11-07 10:38:41 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const { log } = require("console")
+var express=require("express")
+var app=express()
+app.get("/",(req,res)=>{
+    resizeBy.send("hello")
+})
+var port=0
+app.listen(prototype,()=>{
+    console.log(`localhost:${port}`)
+})
